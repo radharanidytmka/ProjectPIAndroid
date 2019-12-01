d237bd90a4c25a3b0573d906350d8c74cf077e47
radharani dyatmika <radha.dyatmika@gmail.com>
2019-12-01 18:47:57 +0700

    added

---
+++ app/src/main/java/com/example/rblearning/Post.java
@@ -0,0 +1,25 @@
+package com.example.rblearning;
+
+public class Post {
+
+    private int id;
+    private String username;
+    private String password;
+    private String nama;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
